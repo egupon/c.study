697c974e310fc67b5557b56996530355b6653ad4
egu <egupon0903@outlook.jp>
2021-05-20 15:57:44 +0900

    10章まで勉強終了

--- study.c
+++ study.c
@@ -259,6 +259,90 @@ int drawline(void){
 //////////////////////////////////////////////////////////////////
 
 
+/* int main(){
+    FILE *fp;
+    fp = fopen("test.txt", "w");
+
+    if(fp == NULL){
+        perror("ファイルオープンに失敗\n");
+        return -1;
+    }else{
+        printf("ファイルを正常にオープンしました\n");
+    }
+
+    fprintf(fp,"初めてのファイル入出力です。\n");
+
+    if(fclose(fp) != 0){
+        perror("ファイルのクローズに失敗\n");
+        return -1;
+    }else{
+        printf("ファイルを正常にクロ―ズしました\n");
+    }
+
+    return 0;
+} */
+//////////////////////////////////////////////////////////////////////////////
+
+
+/* #include <string.h>
+
+int mywrite(FILE *);
+
+int main(){
+    FILE *fp;
+    char filename[256];
+
+    printf("ファイル名（拡張子不要）：");
+    fgets(filename, sizeof(filename), stdin);
+    filename[strlen(filename) - 1] = '\0';
+    strcat(filename, ".txt");
+
+    fp = fopen(filename, "a");
+    if(fp==NULL){
+        perror("ファイルのオープンに失敗しました。\n");
+        return -1;
+    }
+    while(!mywrite(fp));
+
+    fclose(fp);
+    return 0;
+}
+
+int mywrite(FILE *f){
+    char name[16];
+    char tel[16];
+    char yesno[8];
+    char *format = "%-15s %-15s\n";
+
+    printf("氏名：");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+    printf("電話：");
+    fgets(tel, sizeof(tel), stdin);
+    name[strlen(tel) - 1] = '\0';
+
+    printf(format, name, tel);
+    printf("このデータを書き込みますか？(y/n)：");
+    fgets(yesno, sizeof(yesno), stdin);
+    yesno[strlen(yesno) - 1] = '\0';
+    if(strcmp(yesno, "Y") == 0 || (strcmp(yesno, "y") == 0)){
+        fprintf(f, format, name, tel);
+    }
+
+    printf("さらにデータを入力しますか？(y/n)：");
+    fgets(yesno, sizeof(yesno), stdin);
+    yesno[strlen(yesno) - 1] = '\0';
+    if(strcmp(yesno, "N") == 0 || (strcmp(yesno, "n") == 0)){
+        return -1;
+    }
+
+    return 0;
+} */
+/////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 //gcc -fexec-charset=CP932 -o study study.c
 //.\study
